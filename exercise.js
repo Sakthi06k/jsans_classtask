65479c43a68a6207a27ff1c304f2a343d70331f2
Sakthi06k <110223137+Sakthi06k@users.noreply.github.com>
2023-02-20 18:32:19 +0530

    Update exercise.js

--- exercise.js
+++ exercise.js
@@ -1,60 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Js cls task 1</title>
+</head>
+<body>
+    <script>
+        // diffrence between foreach, map, filter and reduce
+        //use map to create a new array by changing square numbers array
+        var arr=[1,2,3,4,5]
+        var arr1=[];
+        arr.map((i,j)=>{
+            arr1.push(i*i);
+        })
+        console.log(arr1);
+        //use filter to filter out country starts with "e"
+        var country=["england","india","europe"]
+        var e=country.filter((i)=>{
+            return i.startsWith('e');
+        })
+        console.log(e);
 
-//1.for-each 
-
-const places=["kerala","coimabatore","manali","canada"];
-places.forEach(function(display){
-    console.log(display);
-})
-
-//2.Map to create array and return square of nos.
-
-const array=[2,3,4];
-let square= array.map((num)=>num*num);
-console.log(square);
-
-//3.country names starts with "e"
-
-const countries=["canada","egypt","england","estonia","german"];
-let result = countries.filter(result=> result.startsWith("e"));
-console.log(result);
-
-//4.Strings separation
-
-const array2=[1,"asdf",34,56,"let",89,"print"]
-function getStringLists(...array2){
-    let values=array2.filter(num=> typeof num=="string")
-    console.log(values);
-}
-getStringLists(...array2);
-
-
-//5.reduce()
-
-const array3=["Estonia ","Finland ","Sweden ","Denmark ","Norway ","Iceland ","are ","north europian ","countries "];
-function concat(string,string1){
-    return string+string1;
-}let final = array3.reduce(concat);
-console.log(final);
-
-//6.
-var human = ["1.602*10^-19", "3.14", "9.8 m/s^2", "97.9 F","100 c"];
- 
-var e = human[0];
-var PI= human[1];
-var gravity = human[2];
-var body_temp = human[3];
-var water_boil=human[4];
- 
-console.log(e);
-console.log(PI);
-console.log(gravity);
-console.log(body_temp);
-console.log(water_boil);
-
-
-
-
-
-
-
-
+        //declare a function called getStringLists which takes an array as a parameter and returns array only with string items.
+        const getStringLists=(i)=>{
+            return console.log(i.toString());
+        }
+        getStringLists(country);
+       //use reduce to cancadinate all the countries and to produce this sentence: Estiona, finland, sweden, denmark, norway and iceland
+       //find out which letter is used many times as initial for a country name from the countries array(eg.finland,figi,france etc.,)
+       var country2=['france','finland','figi','iceland','india','srilanka','america'];
+       var hashmap=[];
+       var j=0;
+       for(var aa=0;aa<country2.length;aa++){
+        hashmap[country2[aa].charCodeAt(0)]++
+       }
+       var max=-1;
+       var t=0;
+       for(var i=0;i<hashmap.length;i++){
+        if(country2[i]>max);
+        max=i;
+       }
+       //now print the value starts with max the max is in ascii value
+       console.log(hashmap);
+       //assain the elements
+    </script>
+</body>
+</html>
